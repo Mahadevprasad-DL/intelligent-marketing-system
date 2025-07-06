5762976284a0ce35a2011380b4b0c62cbf916243
Apoorv <mahadevprasaddl@gmail.com>
2025-07-06 06:34:30 +0530

    test

--- Backend/index.js
+++ Backend/index.js
@@ -16,3 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`✅ Server running on http://localhost:${PORT}`);
 });
+app.use(cors({
+  origin: 'https://intelligent-marketing-system.vercel.app',
+  credentials: true
+}));
